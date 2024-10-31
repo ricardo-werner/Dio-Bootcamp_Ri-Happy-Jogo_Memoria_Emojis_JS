fc9e6f02013931e2f786ec54a59c98a164d75b3c
Ricardo Werner <wernerdive@gmail.com>
2024-10-31 18:30:42 -0300

    feat: add toda a logica das regras de funcionalidade do jogo

---
+++ src/scripts/script.js
@@ -0,0 +1,59 @@
+const emojis = [
+    "🥳",
+    "🥳",
+    "🤑",
+    "🤑",
+    "🥵",
+    "🥵",
+    "🧐",
+    "🧐",
+    "🤭",
+    "🤭",
+    "👀",
+    "👀",
+    "😬",
+    "😬",
+    "😵",
+    "😵",
+    "🥶",
+    "🥶",
+    "🤐",
+    "🤐"
+];
+let openCards = [];
+
+let shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let card = document.createElement("div");
+    card.className = "content";
+    card.innerHTML = shuffledEmojis[i];
+    card.onclick = handleClick;
+    document.querySelector(".game-board").appendChild(card);
+}
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("cardOpen");
+        openCards.push(this);
+    }
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("cardMatched");
+        openCards[1].classList.add("cardMatched");
+    }else {
+        
+        openCards[0].classList.remove("cardOpen");
+        openCards[1].classList.remove("cardOpen");
+    }
+    openCards = [];
+
+    if (document.querySelectorAll(".cardMatched").length === emojis.length) {
+        alert("Você é o vencedor! 🎉🎉🎉");
+    }
+}
