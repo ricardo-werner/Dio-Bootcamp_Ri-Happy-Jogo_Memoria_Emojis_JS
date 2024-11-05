eefc2bc6dfb035910ed0a5df86487b0f67b38e9e
Ricardo Werner <wernerdive@gmail.com>
2024-11-05 11:57:16 -0300

    refactor: alterado a lógica para a funcionalidade do jogo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -11,53 +11,47 @@ const emojis = [
     "🤭",
     "👀",
     "👀",
-    "😬",
-    "😬",
-    "😵",
-    "😵",
-    "🥶",
-    "🥶",
-    "🤐",
-    "🤐",
-    "🤢",
-    "🤢",
-    "😴",
-    "😴"
+    "🤯",
+    "🤯",
+    "🤩",
+    "🤩"
 ];
 let openCards = [];
 
 let shuffledEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < emojis.length; i++) {
-    let card = document.createElement("div");
-    card.className = "gameCard";
-    card.innerHTML = shuffledEmojis[i];
-    card.onclick = handleClick;
-    document.querySelector(".game").appendChild(card);
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffledEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
 }
 
 function handleClick() {
-    if(openCards.length < 2) {
-        this.classList.add("cardOpen");
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
         openCards.push(this);
     }
-    if(openCards.length == 2){
+    if (openCards.length == 2){
         setTimeout(checkMatch, 500);
     }
+
+    console.log(openCards);
 }
 
 function checkMatch() {
-    if(openCards[0].innerHTML === openCards[1].innerHTML) {
-        openCards[0].classList.add("cardMatched");
-        openCards[1].classList.add("cardMatched");
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatched");
+        openCards[1].classList.add("boxMatched");
     }else {
         
-        openCards[0].classList.remove("cardOpen");
-        openCards[1].classList.remove("cardOpen");
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
     }
     openCards = [];
 
-    if (document.querySelectorAll(".cardMatched").length === emojis.length) {
+    if (document.querySelectorAll(".boxMatched").length === emojis.length) {
         alert("Você é o vencedor! 🎉🎉🎉");
     }
 }
